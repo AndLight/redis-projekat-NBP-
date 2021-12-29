f8b900a0361a8d320a1404cb69fa94649dc2a492
AndLight <andrija.mijajlovic92@gmail.com>
2021-12-29 19:13:44 +0100

    remove, checkbox
    Dodao remove and checkbox funkcije

--- app.js
+++ app.js
@@ -43,16 +43,77 @@ app.use(express.static(path.join(__dirname, '/public')));
         // client.rPush('todo', "jedan get");
         // client.rPush('todo', "dva get");
         
-        console.log("lRange: " +
-            client.lRange('todo', 0, 1, function(err, reply){}
-                ).then(reply => {
-                    console.log("REPLY ******* "+reply);          
-                    res.render('home', {tasks: reply});
-                }
-        ));
+        client.lRange('todo', 0, -1, function(err, reply){}
+            ).then(reply => {
+                res.render('home', {tasks: reply});
+            });
     });
 
+    app.post('/task/add', function(req, res){
+        let task = req.body.task;
+        if (task === null || task.match(/^ *$/) !== null){
+            console.log("Prazno polje uneto")
+        }
+        else{
+            client.rPush('todo', task, function(err, reply){});
+        }
+        res.redirect('/');
+    });
+
+    app.post("/task/delete", function(req, res){
+        let del = req.body.task;
+        console.log("del "+ del)
+
+        // console.log("lrange celo " +
+        client.lRange('todo', 0, -1, function(err, todo){
+           
+            // for(let i=0; i< todo.lenth; i++){
+            //     console.log("length: "+ todo.lenth)
+            //     if(del.indexOf(todo[i]> -1)){
+            //         //tasks[i]> -1 if it is in the list
+            //         client.lRem('todo', 0, todo[i], function(){});
+            //     }
+            // }
+            // res.redirect('/');
+        }).then((todo) => {
+            let todoLenth; 
+            // console.log("todo "+todo)
+
+            client.LLEN("todo", function(err, repply){}
+                ).then(reply => {
+                    todoLenth = reply;
+                    // console.log("reply lenth "+reply +" " + todoLenth)
+
+                    // console.log("len "+ todoLenth)
+                        for(let i=0; i< todoLenth; i++){
+                            console.log("length: "+ todoLenth)
+                            if(del.indexOf(todo[i])> -1){
+                                console.log("del.indexOf(todo[i])"+del.indexOf(todo[i]))
+                                console.log("del.indexOf(todo[i]> -1) "+del.indexOf(todo[i]> -1))
+                                console.log("deleting "+ todo[i])
+                                //tasks[i]> -1 if it is in the list
+                                client.lRem('todo', -2, todo[i], function(){});
+                            }
+                        }
+                })
+
+            // console.log("len "+ todoLenth)
+            // for(let i=0; i< todo.lenth; i++){
+            //     console.log("length: "+ todo.lenth)
+            //     if(del.indexOf(todo[i]> -1)){
+            //         //tasks[i]> -1 if it is in the list
+            //         client.lRem('todo', 0, todo[i], function(){});
+            //     }
+            // }
+            console.log("end")
+            res.redirect('/');
+        })
+        // )
+        // res.redirect('/');
+    });
 // End Home Page
+
+
 
 ///////////////////////////////////////////////////////
 
